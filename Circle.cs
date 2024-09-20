333d00c933fb2cab4661c608b15365a9fbe5283d
akabdylk <kabdylkakarnur@gmail.com>
2024-09-21 04:24:16 +0500

    first

---
+++ Circle.cs
@@ -0,0 +1,36 @@
+﻿namespace Packt.Shared;
+
+public class Circle : Shape
+{
+    public Circle() { }
+
+    public Circle(double radius)
+    {
+        this.Height = radius * 2;
+        this.Width = radius * 2;
+    }
+
+    public double Radius
+    {
+        get
+        {
+            return Height / 2;
+        }
+        set
+        {
+            Height = value * 2;
+            Width = value * 2;
+        }
+    }
+
+    public override double Area
+    {
+        get
+        {
+            double radius = Height / 2;
+            return Math.PI * radius * radius;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using Packt.Shared;
+
+Rectangle rect = new(height: 3, width: 4.5);
+WriteLine($"Rectangle H: {rect.Height}, W: {rect.Width}, Area: {rect.Area}");
+
+Square sq = new(5);
+WriteLine($"Square    H: {sq.Height}, W: {sq.Width}, Area: {sq.Area}");
+
+Circle circ = new(radius: 2.5);
+WriteLine($"Circle    H: {circ.Height}, W: {circ.Width}, Area: {circ.Area}");
